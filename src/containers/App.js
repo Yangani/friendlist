6aef3b4616c28c0659d2bd6fd5b9ead20df11f37
F <Fyangani@gmail.com>
2016-01-20 20:54:55 -0600

    Update App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <p>Hey, Festus!</p>
+      <p>Hellow React!</p>
     );
   }
 }
